5574753f24df3d0ad7c25a3e905d03944d25865f
oomten <oomten@gmail.com>
2022-11-15 16:13:01 +0500

    Second Commit

---
+++ StoreSearch/SearchResult.swift
@@ -0,0 +1,13 @@
+//
+//  SearchResult.swift
+//  StoreSearch
+//
+//  Created by Denis Nurislamov on 15.11.2022.
+//
+
+import Foundation
+
+class SearchResult {
+    var name = ""
+    var artistName = ""
+}
--- StoreSearch/SearchViewController.swift
+++ StoreSearch/SearchViewController.swift
@@ -7,13 +7,99 @@
 
 import UIKit
 
+//MARK: - Class
 class SearchViewController: UIViewController {
-
+    
+    //MARK: - Instance Variables
+    var hasSearched = false
+    var searchResults = [SearchResult]()
+    
+    
+    //MARK: - Outlets
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var tableView: UITableView!
+    
+    //MARK: - Views
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        tableView.contentInset = UIEdgeInsets(top: 51, left: 0, bottom: 0, right: 0)
+        
     }
+    
+}
 
+//MARK: - Search Bar Delegate
+extension SearchViewController: UISearchBarDelegate {
+    
+    func position(for bar: UIBarPositioning) -> UIBarPosition {
+        return .topAttached
+    }
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        
+        searchBar.resignFirstResponder()
+        
+        searchResults = []
+        if searchBar.text! != "Justin Bieber" {
+            for i in 0...2 {
+                let searchResult = SearchResult()
+                searchResult.name = String(format: "Fake Result %d for", i)
+                searchResult.artistName = searchBar.text!
+                searchResults.append(searchResult)
+            }
+        }
+        hasSearched = true
+        tableView.reloadData()
+    }
+    
+}
 
+//MARK: - Table View Delegate
+extension SearchViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if !hasSearched {
+            return 0
+        } else if searchResults.count == 0 {
+            return 1
+        } else {
+            return searchResults.count
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cellIdenitifier = "SearchResultCell"
+        
+        var cell = tableView.dequeueReusableCell(withIdentifier: cellIdenitifier)
+        if cell == nil {
+            cell = UITableViewCell(
+                style: .subtitle, reuseIdentifier: cellIdenitifier)
+        }
+        if searchResults.count == 0 {
+            cell?.textLabel!.text = "(Nothing found)"
+            cell?.detailTextLabel!.text = ""
+        } else {
+            let searchResult = searchResults[indexPath.row]
+            cell?.textLabel!.text = searchResult.name
+            cell?.detailTextLabel!.text = searchResult.artistName
+        }
+        return cell!
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    func tableView(_ tableView: UITableView, willSelectRowAt indexPath: IndexPath) -> IndexPath? {
+        if searchResults.count == 0 {
+            return nil
+        } else {
+            return indexPath
+        }
+    }
+    
+    
 }
 
